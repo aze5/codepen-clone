108cc1c8077eaf3bb1f7c0750ed83c0d03c3147f
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-05-08 18:10:17 +0100

    setup codemirror in editor

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
+import { html } from '@codemirror/lang-html';
+import { css } from '@codemirror/lang-css';
+import { sublime } from '@uiw/codemirror-theme-sublime';
+import { EditorView } from '@codemirror/view';
 
-export default function Editor() {
+export default function Editor(props) {
+  const {
+    displayName,
+    language,
+    onChange,
+    value
+  } = props
+
+  function handleChange(value, viewUpdate) {
+    onChange(value)
+  }
+  
   return (
-    <div>Editor</div>
+    <div className="editor-container">
+      <div className="editor-title">
+        {displayName}
+        <button>O/C</button>
+      </div>
+      <CodeMirror 
+        onChange={handleChange}
+        value={value}
+        className='codemirror-wrapper'
+        extension={[language, EditorView.lineWrapping]}
+        theme={sublime}
+      />
+    </div>
   )
 }
 
